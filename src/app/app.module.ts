b17ae9a5a5d78cacd35228da57e39b014500831a
Jeremy Little <jeremy.little@mercenary-coders.com>
2024-12-05 12:45:08 -0600

    clean-slate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyFormComponent } from './my-form/my-form.component';
-import { AppNameEditorComponent } from './app-name-editor/app-name-editor.component';
-import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFormComponent,
-    AppNameEditorComponent,
-    ProfileEditorComponent
   ],
   imports: [
     BrowserModule,
